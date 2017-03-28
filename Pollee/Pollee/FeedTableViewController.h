5c17291f145a81ac8e93f34e249176aa39de0541
Your Name <you@example.com>
2017-03-28 15:48:50 -0400

    building out feed framework

---
+++ Pollee/Pollee/FeedTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedTableViewController.h
+//  Pollee
+//
+//  Created by Alex Krush on 3/28/17.
+//  Copyright © 2017 Alex Krush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedTableViewController : UITableViewController
+
+@end
